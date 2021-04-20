e9aad418afab057b5dcc29290ba593c421037ad1
SiurinaiSinghal <75969587+SiurinaiSinghal@users.noreply.github.com>
2021-04-20 18:51:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+car1_width = 120;
+car1_height = 70;
+car1_image = "car1.png";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "car2.png";
+car2_x = 10;
+car2_y = 100;
+
+background_imgage = "canvas-ground.jpg"
+
+function add(){
+    
+}
